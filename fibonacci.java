e2a2a787b1ebe19125125d1fe49192cadf765805
Ismat <ismetsemedov@gmail.com>
2024-01-28 01:29:35 +0400

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Fibonacci {
 
-    // Function to generate Fibonacci sequence up to n terms
     public static List<Integer> generateFibonacci(int n) {
         List<Integer> fibonacciSequence = new ArrayList<>();
         int first = 0, second = 1, next;
@@ -23,15 +22,12 @@ public class Fibonacci {
         Scanner scanner = new Scanner(System.in);
         int n;
 
-        // Input the number of terms for the Fibonacci sequence
         System.out.print("Enter the number of terms for the Fibonacci sequence: ");
         n = scanner.nextInt();
 
-        // Check for valid input
         if (n <= 0) {
             System.out.println("Please enter a positive integer for the number of terms.");
         } else {
-            // Generate and print the Fibonacci sequence
             List<Integer> fibonacciSequence = generateFibonacci(n);
             System.out.print("Fibonacci Sequence: ");
             for (int num : fibonacciSequence) {
